3b1dad8c83c1d6f1ce009e5022ffe94c4daafef5
Alex Silva <alexaraujo_rj@yahoo.com.br>
2021-11-23 13:16:36 -0300

    first commit

---
+++ Enums/Enums.java
@@ -0,0 +1,21 @@
+
+
+public enum Prioridade{
+    MIN,NORMAL,MAX;
+}
+
+//
+
+public enum Prioridade{
+    MIN(1),NORMAL(5),MAX(10);
+
+    private int valor;
+
+    Prioridade(int valor){
+        this.valor = valor;
+    }
+
+    public int getValor(){
+        return this.valor;
+    }
+}
